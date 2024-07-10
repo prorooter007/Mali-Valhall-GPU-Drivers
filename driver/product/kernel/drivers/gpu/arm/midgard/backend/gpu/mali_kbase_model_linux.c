xxmustafacooTR/bifrost-drivers

Add Samsung Changes

driver/product/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_model_linux.c
@@ -158,7 +158,7 @@ KBASE_EXPORT_TEST_API(kbase_set_custom_irq_handler);
 
 int kbase_gpu_device_create(struct kbase_device *kbdev)
 {
-	kbdev->model = midgard_model_create(kbdev);
+	kbdev->model = bv_r48p0_model_create(kbdev);
 	if (kbdev->model == NULL)
 		return -ENOMEM;
 
@@ -174,5 +174,5 @@ int kbase_gpu_device_create(struct kbase_device *kbdev)
  */
 void kbase_gpu_device_destroy(struct kbase_device *kbdev)
 {
-	midgard_model_destroy(kbdev->model);
+	bv_r48p0_model_destroy(kbdev->model);
 }

driver/product/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_model_linux.h
@@ -55,7 +55,7 @@
  *
  * @kbdev: Kbase device.
  *
- * Specific model hook is implemented by midgard_model_create()
+ * Specific model hook is implemented by bv_r48p0_model_create()
  *
  * Return: 0 on success, error code otherwise.
  */
@@ -66,12 +66,12 @@ int kbase_gpu_device_create(struct kbase_device *kbdev);
  *
  * @kbdev: Kbase device.
  *
- * Specific model hook is implemented by midgard_model_destroy()
+ * Specific model hook is implemented by bv_r48p0_model_destroy()
  */
 void kbase_gpu_device_destroy(struct kbase_device *kbdev);
 
 /**
- * midgard_model_create() - Private create function.
+ * bv_r48p0_model_create() - Private create function.
  *
  * @kbdev: Kbase device.
  *
@@ -79,19 +79,19 @@ void kbase_gpu_device_destroy(struct kbase_device *kbdev);
  *
  * Return: Model handle.
  */
-void *midgard_model_create(struct kbase_device *kbdev);
+void *bv_r48p0_model_create(struct kbase_device *kbdev);
 
 /**
- * midgard_model_destroy() - Private destroy function.
+ * bv_r48p0_model_destroy() - Private destroy function.
  *
  * @h: Model handle.
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_destroy(void *h);
+void bv_r48p0_model_destroy(void *h);
 
 /**
- * midgard_model_write_reg() - Private model write function.
+ * bv_r48p0_model_write_reg() - Private model write function.
  *
  * @h: Model handle.
  * @addr: Address at which to write.
@@ -99,10 +99,10 @@ void midgard_model_destroy(void *h);
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_write_reg(void *h, u32 addr, u32 value);
+void bv_r48p0_model_write_reg(void *h, u32 addr, u32 value);
 
 /**
- * midgard_model_read_reg() - Private model read function.
+ * bv_r48p0_model_read_reg() - Private model read function.
  *
  * @h: Model handle.
  * @addr: Address from which to read.
@@ -110,7 +110,7 @@ void midgard_model_write_reg(void *h, u32 addr, u32 value);
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_read_reg(void *h, u32 addr, u32 *const value);
+void bv_r48p0_model_read_reg(void *h, u32 addr, u32 *const value);
 
 /**
  * gpu_device_raise_irq() - Private IRQ raise function.

driver/product/kernel/drivers/gpu/arm/midgard/csf/mali_kbase_csf_firmware.h
@@ -25,7 +25,7 @@
 #include "device/mali_kbase_device.h"
 #include <csf/mali_kbase_csf_registers.h>
 #include <hw_access/mali_kbase_hw_access_regmap.h>
-#include <uapi/gpu/arm/midgard/gpu/mali_kbase_gpu_regmap.h>
+#include <uapi/gpu/arm/bv_r48p0/gpu/mali_kbase_gpu_regmap.h>
 
 /*
  * PAGE_KERNEL_RO was only defined on 32bit ARM in 4.19 in:

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_gpuprops_types.h
@@ -26,7 +26,7 @@
 #ifndef _KBASE_GPUPROPS_TYPES_H_
 #define _KBASE_GPUPROPS_TYPES_H_
 
-#include <uapi/gpu/arm/midgard/mali_base_kernel.h>
+#include <uapi/gpu/arm/bv_r48p0/mali_base_kernel.h>
 
 #define KBASE_GPU_SPEED_MHZ 123
 #define KBASE_GPU_PC_SIZE_LOG2 24U

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_jm.h
@@ -63,7 +63,7 @@
 #ifndef _KBASE_KINSTR_JM_H_
 #define _KBASE_KINSTR_JM_H_
 
-#include <uapi/gpu/arm/midgard/mali_kbase_kinstr_jm_reader.h>
+#include <uapi/gpu/arm/bv_r48p0/mali_kbase_kinstr_jm_reader.h>
 
 #ifdef __KERNEL__
 #include <linux/version.h>

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_prfcnt.c
@@ -23,7 +23,7 @@
 #include "mali_kbase_kinstr_prfcnt.h"
 #include "hwcnt/mali_kbase_hwcnt_virtualizer.h"
 #include "hwcnt/mali_kbase_hwcnt_gpu.h"
-#include <uapi/gpu/arm/midgard/mali_kbase_ioctl.h>
+#include <uapi/gpu/arm/bv_r48p0/mali_kbase_ioctl.h>
 #include "mali_malisw.h"
 #include "mali_kbase_debug.h"
 

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_prfcnt.h
@@ -27,7 +27,7 @@
 #define _KBASE_KINSTR_PRFCNT_H_
 
 #include "hwcnt/mali_kbase_hwcnt_types.h"
-#include <uapi/gpu/arm/midgard/mali_kbase_hwcnt_reader.h>
+#include <uapi/gpu/arm/bv_r48p0/mali_kbase_hwcnt_reader.h>
 
 struct kbase_kinstr_prfcnt_context;
 struct kbase_kinstr_prfcnt_client;

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_mem.h
@@ -31,7 +31,7 @@
 #endif
 
 #include <hw_access/mali_kbase_hw_access_regmap.h>
-#include <uapi/gpu/arm/midgard/mali_base_kernel.h>
+#include <uapi/gpu/arm/bv_r48p0/mali_base_kernel.h>
 #include <mali_kbase_hw.h>
 #include "mali_kbase_pm.h"
 #include "mali_kbase_defs.h"
@@ -41,8 +41,13 @@
 #include "mali_kbase_mem_migrate.h"
 
 #include <linux/version_compat_defs.h>
-#include <linux/sched/mm.h>
 #include <linux/kref.h>
+
+#if KERNEL_VERSION(4, 14, 0) < LINUX_VERSION_CODE
+#include <linux/sched/mm.h>
+#else
+#include <linux/sched.h>
+#endif
 
 static inline void kbase_process_page_usage_inc(struct kbase_context *kctx, int pages);
 

prorooter007/Mali-Valhall-GPU-Drivers

Signed-off-by: Shreyash Wasnik <shreyashwasnik112@gmail.com>

driver/product/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_model_linux.c
@@ -158,7 +158,7 @@ KBASE_EXPORT_TEST_API(kbase_set_custom_irq_handler);
 
 int kbase_gpu_device_create(struct kbase_device *kbdev)
 {
-	kbdev->model = midgard_model_create(kbdev);
+	kbdev->model = bv_r49p0_model_create(kbdev);
 	if (kbdev->model == NULL)
 		return -ENOMEM;
 
@@ -174,5 +174,5 @@ int kbase_gpu_device_create(struct kbase_device *kbdev)
  */
 void kbase_gpu_device_destroy(struct kbase_device *kbdev)
 {
-	midgard_model_destroy(kbdev->model);
+	bv_r49p0_model_destroy(kbdev->model);
 }

driver/product/kernel/drivers/gpu/arm/midgard/backend/gpu/mali_kbase_model_linux.h
@@ -55,7 +55,7 @@
  *
  * @kbdev: Kbase device.
  *
- * Specific model hook is implemented by midgard_model_create()
+ * Specific model hook is implemented by bv_r49p0_model_create()
  *
  * Return: 0 on success, error code otherwise.
  */
@@ -66,12 +66,12 @@ int kbase_gpu_device_create(struct kbase_device *kbdev);
  *
  * @kbdev: Kbase device.
  *
- * Specific model hook is implemented by midgard_model_destroy()
+ * Specific model hook is implemented by bv_r49p0_model_destroy()
  */
 void kbase_gpu_device_destroy(struct kbase_device *kbdev);
 
 /**
- * midgard_model_create() - Private create function.
+ * bv_r49p0_model_create() - Private create function.
  *
  * @kbdev: Kbase device.
  *
@@ -79,19 +79,19 @@ void kbase_gpu_device_destroy(struct kbase_device *kbdev);
  *
  * Return: Model handle.
  */
-void *midgard_model_create(struct kbase_device *kbdev);
+void *bv_r49p0_model_create(struct kbase_device *kbdev);
 
 /**
- * midgard_model_destroy() - Private destroy function.
+ * bv_r49p0_model_destroy() - Private destroy function.
  *
  * @h: Model handle.
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_destroy(void *h);
+void bv_r49p0_model_destroy(void *h);
 
 /**
- * midgard_model_write_reg() - Private model write function.
+ * bv_r49p0_model_write_reg() - Private model write function.
  *
  * @h: Model handle.
  * @addr: Address at which to write.
@@ -99,10 +99,10 @@ void midgard_model_destroy(void *h);
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_write_reg(void *h, u32 addr, u32 value);
+void bv_r49p0_model_write_reg(void *h, u32 addr, u32 value);
 
 /**
- * midgard_model_read_reg() - Private model read function.
+ * bv_r49p0_model_read_reg() - Private model read function.
  *
  * @h: Model handle.
  * @addr: Address from which to read.
@@ -110,7 +110,7 @@ void midgard_model_write_reg(void *h, u32 addr, u32 value);
  *
  * This hook is specific to the model built in Kbase.
  */
-void midgard_model_read_reg(void *h, u32 addr, u32 *const value);
+void bv_r49p0_model_read_reg(void *h, u32 addr, u32 *const value);
 
 /**
  * gpu_device_raise_irq() - Private IRQ raise function.

driver/product/kernel/drivers/gpu/arm/midgard/csf/mali_kbase_csf_firmware.h
@@ -25,7 +25,7 @@
 #include "device/mali_kbase_device.h"
 #include <csf/mali_kbase_csf_registers.h>
 #include <hw_access/mali_kbase_hw_access_regmap.h>
-#include <uapi/gpu/arm/midgard/gpu/mali_kbase_gpu_regmap.h>
+#include <uapi/gpu/arm/bv_r49p0/gpu/mali_kbase_gpu_regmap.h>
 
 /*
  * PAGE_KERNEL_RO was only defined on 32bit ARM in 4.19 in:

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_gpuprops_types.h
@@ -26,7 +26,7 @@
 #ifndef _KBASE_GPUPROPS_TYPES_H_
 #define _KBASE_GPUPROPS_TYPES_H_
 
-#include <uapi/gpu/arm/midgard/mali_base_kernel.h>
+#include <uapi/gpu/arm/bv_r49p0/mali_base_kernel.h>
 
 #define KBASE_GPU_SPEED_MHZ 123
 #define KBASE_GPU_PC_SIZE_LOG2 24U

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_jm.h
@@ -63,7 +63,7 @@
 #ifndef _KBASE_KINSTR_JM_H_
 #define _KBASE_KINSTR_JM_H_
 
-#include <uapi/gpu/arm/midgard/mali_kbase_kinstr_jm_reader.h>
+#include <uapi/gpu/arm/bv_r49p0/mali_kbase_kinstr_jm_reader.h>
 
 #ifdef __KERNEL__
 #include <linux/version.h>

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_prfcnt.c
@@ -23,7 +23,7 @@
 #include "mali_kbase_kinstr_prfcnt.h"
 #include "hwcnt/mali_kbase_hwcnt_virtualizer.h"
 #include "hwcnt/mali_kbase_hwcnt_gpu.h"
-#include <uapi/gpu/arm/midgard/mali_kbase_ioctl.h>
+#include <uapi/gpu/arm/bv_r49p0/mali_kbase_ioctl.h>
 #include "mali_malisw.h"
 #include "mali_kbase_debug.h"
 

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_kinstr_prfcnt.h
@@ -27,7 +27,7 @@
 #define _KBASE_KINSTR_PRFCNT_H_
 
 #include "hwcnt/mali_kbase_hwcnt_types.h"
-#include <uapi/gpu/arm/midgard/mali_kbase_hwcnt_reader.h>
+#include <uapi/gpu/arm/bv_r49p0/mali_kbase_hwcnt_reader.h>
 
 struct kbase_kinstr_prfcnt_context;
 struct kbase_kinstr_prfcnt_client;

driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_mem.h
@@ -31,7 +31,7 @@
 #endif
 
 #include <hw_access/mali_kbase_hw_access_regmap.h>
-#include <uapi/gpu/arm/midgard/mali_base_kernel.h>
+#include <uapi/gpu/arm/bv_r49p0/mali_base_kernel.h>
 #include <mali_kbase_hw.h>
 #include "mali_kbase_pm.h"
 #include "mali_kbase_defs.h"
@@ -41,8 +41,13 @@
 #include "mali_kbase_mem_migrate.h"
 
 #include <linux/version_compat_defs.h>
-#include <linux/sched/mm.h>
 #include <linux/kref.h>
+
+#if KERNEL_VERSION(4, 14, 0) < LINUX_VERSION_CODE
+#include <linux/sched/mm.h>
+#else
+#include <linux/sched.h>
+#endif
 
 static inline void kbase_process_page_usage_inc(struct kbase_context *kctx, int pages);
 